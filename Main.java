4d1be2bb9f9e55a030d85d95af4d85530b80b064
Tanishka <anwekar.tanu2019@gmail.com>
2025-12-28 20:34:15 +0530

    Pactice 2 : Inheritance

--- Main.java
+++ Main.java
@@ -1,40 +1,22 @@
-class Student 
-{
-    int rollno;
-    String name;
-    int marks;
+
+class Animal {
+    void eat() {
+        System.out.println("This animal is eating.");
+    }
 }
 
 
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog is barking.");
+    }
+}
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.rollno = 1;
-        s1.name = "tanu";
-        s1.marks = 100;
-
-        Student s2 = new Student();
-        s2.rollno = 2;
-        s2.name = "ganu";
-        s2.marks = 300;
-
-        Student s3 = new Student();
-        s3.rollno = 3;
-        s3.name = "manu";
-        s3.marks = 200;
-
-        Student students[] = new Student[3]; // array which can hold student references
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-
-        for(int i = 0; i<students.length; i++)
-        {
-            System.out.println(students[i].rollno + " :" + students[i].name + " :" + students[i].marks );
-        }
-
-       
+         
+        Dog myDog = new Dog();
+        myDog.eat(); 
+        myDog.bark();
     }
 }
